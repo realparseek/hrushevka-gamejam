48a02bcdf956cf168b44e10b0016b0bcf1416e6e
realparseek <kadyshev74@gmail.com>
2025-10-26 19:37:00 +0500

    basic vorona's ai

---
+++ scripts/states/vorona/idle.gd
@@ -0,0 +1,27 @@
+extends State
+
+@export var gravity: float = 9.8
+@export var anim_player: AnimationPlayer = null
+
+@onready var cbody: CharacterBody3D = $"../.."
+@onready var cbody_player: CharacterBody3D = $"../../../player_fps"
+
+func enter() -> void:
+	pass
+	
+func exit() -> void:
+	pass
+
+func update(delta: float) -> void:
+	float(delta)
+	pass
+
+func physics_update(delta: float) -> void:
+	if cbody_player.global_position.distance_to(cbody.global_position) <= 5.0:
+		Transitioned.emit("walking", self)
+	
+	if !anim_player.is_playing():
+		anim_player.play("idle")
+	
+	if !cbody.is_on_floor():
+		cbody.velocity.y -= gravity
---
+++ scripts/states/vorona/walking.gd
@@ -0,0 +1,32 @@
+extends State
+
+@export var agent: NavigationAgent3D = null
+@export var move_speed: float = 1.0
+@export var gravity: float = 9.8
+@export var target3d: Vector3 = Vector3.ZERO
+
+@onready var cbody: CharacterBody3D = $"../.."
+@onready var cbody_player: CharacterBody3D = $"../../../player_fps"
+
+func enter() -> void:
+	agent.target_position = target3d
+	
+func exit() -> void:
+	pass
+
+func update(delta: float) -> void:
+	float(delta)
+	pass
+
+func physics_update(delta: float) -> void:
+	agent.target_position = cbody_player.global_position
+	if agent.is_target_reached():
+		return
+	
+	var nextp: Vector3 = agent.get_next_path_position()
+	if nextp != cbody.global_position:
+		cbody.look_at(nextp, Vector3.UP, true)
+		cbody.rotation.x = 0.0
+	
+	cbody.velocity = (Vector3(nextp.x, 0.0, nextp.z) - cbody.position).normalized() * move_speed
+	cbody.move_and_slide()
