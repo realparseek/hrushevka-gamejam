c6de4aa4a980f294e231bcda399d1e779c1ac768
realparseek <kadyshev74@gmail.com>
2025-10-26 21:11:40 +0500

    Smoother Vorona's movement

--- scripts/states/vorona/idle.gd
+++ scripts/states/vorona/idle.gd
@@ -1,27 +1,33 @@
 extends State
 
 @export var gravity: float = 9.8
-@export var anim_player: AnimationPlayer = null
+@export var anim_tree: AnimationTree = null
+@export var blend_weight: float = 0.05
 
 @onready var cbody: CharacterBody3D = $"../.."
 @onready var cbody_player: CharacterBody3D = $"../../../player_fps"
 
+@onready var walk_val: float = 0.0
+
 func enter() -> void:
-	pass
+	walk_val = anim_tree["parameters/Walk/blend_amount"]
 	
 func exit() -> void:
-	anim_player.stop()
+	pass
 
 func update(delta: float) -> void:
 	float(delta)
 	pass
 
 func physics_update(delta: float) -> void:
+	walk_val = lerpf(walk_val, 0.0, blend_weight)
+	update_anim_tree()
+	
 	if cbody_player.global_position.distance_to(cbody.global_position) <= 5.0:
 		Transitioned.emit("walking", self)
 	
-	if !anim_player.is_playing():
-		anim_player.play("idle")
-	
 	if !cbody.is_on_floor():
 		cbody.velocity.y -= gravity
+
+func update_anim_tree() -> void:
+	anim_tree["parameters/Walk/blend_amount"] = walk_val
--- scripts/states/vorona/walking.gd
+++ scripts/states/vorona/walking.gd
@@ -3,35 +3,42 @@ extends State
 @export var agent: NavigationAgent3D = null
 @export var move_speed: float = 1.0
 @export var gravity: float = 9.8
-@export var anim_player: AnimationPlayer = null
+@export var anim_tree: AnimationTree = null
 @export var target3d: Vector3 = Vector3.ZERO
+@export var blend_weight: float = 0.05
 
 @onready var cbody: CharacterBody3D = $"../.."
 @onready var cbody_player: CharacterBody3D = $"../../../player_fps"
+@onready var walk_val: float = 0.0
 
 func enter() -> void:
 	agent.target_position = target3d
+	walk_val = anim_tree["parameters/Walk/blend_amount"]
 	
 func exit() -> void:
-	anim_player.stop()
+	pass
 
 func update(delta: float) -> void:
 	float(delta)
 	pass
 
 func physics_update(delta: float) -> void:
+	walk_val = lerpf(walk_val, 1.0, blend_weight)
+	update_anim_tree()
+	
 	agent.target_position = cbody_player.global_position
 	if agent.is_target_reached():
-		anim_player.stop()
 		return
-		
-	if !anim_player.is_playing():
-		anim_player.play("walk")
 	
 	var nextp: Vector3 = agent.get_next_path_position()
 	if nextp != cbody.global_position:
-		cbody.look_at(nextp, Vector3.UP, true)
-		cbody.rotation.x = 0.0
+		var pos2d: Vector2 = Vector2(cbody.global_position.x, cbody.global_position.z)
+		var player_pos2d: Vector2 = Vector2(nextp.x, nextp.z)
+		var dir: Vector2 = -(pos2d - player_pos2d)
+		cbody.rotation.y = lerp_angle(cbody.rotation.y, atan2(dir.x, dir.y), blend_weight)
 	
 	cbody.velocity = (Vector3(nextp.x, 0.0, nextp.z) - cbody.position).normalized() * move_speed
 	cbody.move_and_slide()
+
+func update_anim_tree() -> void:
+	anim_tree["parameters/Walk/blend_amount"] = walk_val
