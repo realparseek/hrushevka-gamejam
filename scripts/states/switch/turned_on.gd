af9d5b1a63464648c5c432fbea82fb5d47437328
realparseek <kadyshev74@gmail.com>
2025-11-03 01:02:38 +0500

    Fixed floor Z-fighting

---
+++ scripts/states/switch/turned_on.gd
@@ -0,0 +1,15 @@
+extends State
+
+func enter() -> void:
+	pass
+	
+func exit() -> void:
+	pass
+
+func update(delta: float) -> void:
+	float(delta)
+	pass
+
+func physics_update(delta: float) -> void:
+	float(delta)
+	pass
