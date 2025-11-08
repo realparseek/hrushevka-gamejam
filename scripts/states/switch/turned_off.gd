87e111bd5a3c1832dfb081ed666d74bbd60c1350
realparseek <kadyshev74@gmail.com>
2025-11-09 04:01:09 +0500

    added light switching on spacebar

--- scripts/states/switch/turned_off.gd
+++ scripts/states/switch/turned_off.gd
@@ -1,12 +1,20 @@
 extends State
 
+@export var light_sources: Array[State]
+@export var environment: WorldEnvironment
+
 func enter() -> void:
-	pass
+	for ls in light_sources:
+		ls.Transitioned.emit("turnedoff", ls.get_parent().current_state)
+	environment.environment.ambient_light_color = Color(0.153, 0.153, 0.153)
 	
 func exit() -> void:
 	pass
 
 func update(delta: float) -> void:
+	if Input.is_action_just_pressed("jump"):
+		Transitioned.emit("turnedon", self)
+	
 	float(delta)
 	pass
 
--- scripts/states/switch/turned_on.gd
+++ scripts/states/switch/turned_on.gd
@@ -1,12 +1,20 @@
 extends State
 
+@export var light_sources: Array[State]
+@export var environment: WorldEnvironment
+
 func enter() -> void:
-	pass
+	for ls in light_sources:
+		ls.Transitioned.emit("turnedon", ls.get_parent().current_state)
+	environment.environment.ambient_light_color = Color(0.647, 0.516, 0.413)
 	
 func exit() -> void:
 	pass
 
 func update(delta: float) -> void:
+	if Input.is_action_just_pressed("jump"):
+		Transitioned.emit("turnedoff", self)
+	
 	float(delta)
 	pass
 
