a3f5c40c323948805534dfe0ce1290519acb53fb
realparseek <kadyshev74@gmail.com>
2026-01-29 08:50:52 +0500

    extended enterance

--- scripts/interactions/documents/read.gd
+++ scripts/interactions/documents/read.gd
@@ -23,7 +23,7 @@ extends Interaction
 	+ '3. Рекомендовать Главному управлению ММВД Краславии по Капканской области (тов. В.С. Громов) '
 	+ 'обеспечить, совместно с воинскими подразделениями, охрану общественного порядка и соблюдение '
 	+ 'пропускного режима на территории Зоны Отселения.\n\n'
-	+ '4. правлению жилищно-коммунального хозяйства (тов. А.И. Сазонов) в трехдневный срок: '
+	+ '4. Правлению жилищно-коммунального хозяйства (тов. А.И. Сазонов) в трехдневный срок: '
 	+ '4.1. Определить перечень свободного жилого фонда, пригодного для временного размещения '
 	+ 'переселенцев.\n\n'
 	+ '4.2. Организовать предоставление гражданам, подлежащим переселению, жилых помещений '
@@ -39,6 +39,26 @@ extends Interaction
 	+ '7. Контроль за исполнением настоящего распоряжения возложить на первого заместителя '
 	+ 'губернатора Капканской области тов. П.К. Загорского.\n'
 )
+
+#@onready var text: String = (
+	#"REPUBLIC OF KRASLAVIA\n"
+	#+ "KAPKANSK REGION\n\n"
+	#+ "ORDER\n\n"
+	#+ "On urgent measures for relocating citizens residing in the Metallurgical District of the city of Kapkansk due to a natural emergency. "
+	#+ "Due to the occurrence of a natural emergency that resulted in the release into the atmosphere of an unidentified highly toxic gas (code designation \"V-04\") posing an immediate threat to the life and health of the population, and based on the conclusion of the Interdepartmental Sanitary-Chemical Commission No. 42-SK dated \"27\" April 2011,\n\n"
+	#+ "I ORDER:\n\n"
+	#+ "1. To deem it necessary to carry out the immediate planned evacuation and subsequent relocation of citizens permanently or temporarily residing in the territory of the Metallurgical District of the municipal formation \"City of Kapkansk\" (hereinafter – the Evacuation Zone).\n\n"
+	#+ "2. To approve the boundaries of the Evacuation Zone in accordance with Annex No. 1 to this order. Establish that presence of citizens within the Evacuation Zone is prohibited from \"28\" April 2011.\n\n"
+	#+ "3. To recommend that the Main Directorate of the Ministry of Internal Affairs of Kraslavia for the Kapkansk Region (Comrade V.S. Gromov) ensure, together with military units, the protection of public order and enforcement of access control within the Evacuation Zone.\n\n"
+	#+ "4. To the Housing and Communal Services Administration (Comrade A.I. Sazonov), within three days:\n"
+	#+ "4.1. Determine the list of available housing stock suitable for temporary accommodation of evacuees.\n"
+	#+ "4.2. Organize provision of housing from the maneuver fund in the city of Kapkansk and neighboring settlements to citizens subject to relocation.\n\n"
+	#+ "5. To the Department of Health (Comrade L.P. Orlova):\n\n"
+	#+ "5.1. Organize mandatory medical examinations of all citizens exposed to gas \"V-04\".\n\n"
+	#+ "5.2. Ensure immediate hospitalization of persons showing signs of acute and chronic poisoning.\n\n"
+	#+ "6. Announce that the evacuation is a temporary measure. The question of the timing and conditions of return, as well as compensation for lost property, will be considered by a special commission after the complete elimination of the threat and issuance of a safety conclusion.\n\n"
+	#+ "7. Assign control over the implementation of this order to the First Deputy Governor of the Kapkansk Region, Comrade P.K. Zagorsky.\n"
+#)
 
 @onready var texture: Texture2D
 
