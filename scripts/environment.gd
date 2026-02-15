9bdac92fc279dcde9260ad5dc10b90c6dd3a7828
realparseek <kadyshev74@gmail.com>
2026-02-15 23:36:58 +0500

    smooth ambience change

--- scripts/environment.gd
+++ scripts/environment.gd
@@ -3,4 +3,4 @@ extends WorldEnvironment
 @onready var ambient_energy: float = 1.0
 
 func _process(delta: float) -> void:
-	environment.ambient_light_energy = lerp(environment.ambient_light_energy, ambient_energy, delta*2)
+	environment.ambient_light_energy = lerp(environment.ambient_light_energy, ambient_energy, delta*0.5)
