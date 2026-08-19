4f11dcd9296e903389961e5b634ec6fbc500328a
realparseek <kadyshev74@gmail.com>
2026-08-20 04:10:35 +0500

    ui: resizing ui window

--- scripts/ui_window/ui_window.gd
+++ scripts/ui_window/ui_window.gd
@@ -4,22 +4,34 @@ class_name UIWindow
 @export var rect: NinePatchRect
 
 var DRAGGING: bool = false
+var RESIZING: bool = false
 var WSTARTPOS: Vector2 = Vector2.ZERO
 var MSTARTPOS: Vector2 = Vector2.ZERO
+var WSTARTSIZE: Vector2 = Vector2.ZERO
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed:
 			var wrect = Rect2(rect.global_position, rect.size)
-			if wrect.has_point(event.position):
+			var rrect = Rect2(wrect.position+wrect.size-Vector2(25,25), Vector2(25,25))
+			if rrect.has_point(event.position):
+				RESIZING = true
+				WSTARTSIZE = wrect.size
+				MSTARTPOS = event.position
+			elif wrect.has_point(event.position):
 				DRAGGING = true
 				WSTARTPOS = wrect.position
 				MSTARTPOS = event.position
+				Input.set_default_cursor_shape(Input.CURSOR_BDIAGSIZE)
 		else:
 			DRAGGING = false
+			RESIZING = false
 	
-	if event is InputEventMouseMotion and DRAGGING:
-		rect.global_position = WSTARTPOS + event.position - MSTARTPOS
+	if event is InputEventMouseMotion:
+		if DRAGGING:
+			rect.global_position = WSTARTPOS + event.position - MSTARTPOS
+		elif RESIZING:
+			rect.size = WSTARTSIZE + event.position - MSTARTPOS
 		
 	var rwsize = get_window().size
 	rect.global_position.x = clamp(rect.global_position.x, -25, rwsize.x-25)
