ab3d983329b67434a6680b148c73cb1238997e8f
realparseek <kadyshev74@gmail.com>
2026-08-23 10:32:27 +0500

    pause menu: ambient playback looping

--- scripts/pause_menu/pause_menu.gd
+++ scripts/pause_menu/pause_menu.gd
@@ -4,8 +4,10 @@ class_name PauseMenu
 @export var menu: ColorRect
 @export var resume_button: Button
 @export var exit_button: Button
+@export var ambient: AudioStreamPlayer
 
 var PAUSED: bool = false
+var AMBIENTPOS: float = 0.0
 
 func _ready() -> void:
 	if resume_button: resume_button.connect("pressed", _on_resume_btn_press)
@@ -19,13 +21,19 @@ func pause() -> void:
 	get_tree().paused = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	menu.visible = true
-	PAUSED = true
+	
+	if ambient:
+		ambient.play(AMBIENTPOS)
+		PAUSED = true
 
 func unpause() -> void:
 	get_tree().paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	menu.visible = false
+	AMBIENTPOS = ambient.get_playback_position()
 	PAUSED = false
+	
+	if ambient: ambient.stop()
 
 func _on_resume_btn_press() -> void:
 	unpause()
